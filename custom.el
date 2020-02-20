a97e0da57e1d23c9ff219c3f8be664790a7bcc94
Paul Heely <pheely@amazon.com>
2020-02-20 12:40:43 -0500

    Updates and added .gitignore.

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (exec-path-from-shell use-package magit))))
+ '(package-selected-packages
+   (quote
+    (evil leuven-theme gruvbox-theme magit doom-themes markdown-mode yaml-mode exec-path-from-shell use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
